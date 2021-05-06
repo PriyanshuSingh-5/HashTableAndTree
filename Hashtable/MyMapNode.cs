1eacbd72ebc600552d0a2d5ae236fde36e42c840
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-06 14:34:10 +0530

    [Priyanshu]modified a sequence of words

--- Hashtable/MyMapNode.cs
+++ Hashtable/MyMapNode.cs
@@ -6,122 +6,114 @@ using System.Threading.Tasks;
 
 namespace Hashtable
 {
-   
-        public class MyMapNode<K, V>
+
+    public class MyMapNode<K, V>  //initializing the hashtable
+    {
+        private readonly int size;
+        
+        private readonly LinkedList<keyValue<K, V>>[] items;
+
+        
+        public MyMapNode(int size)
         {
-            public struct keyValue<k, v>
+            this.size = size;
+            //array of linkedlist size is defined
+            this.items = new LinkedList<keyValue<K, V>>[size];
+        }
+       
+        protected int GetArrayPosition(K key)
+        {
+           
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+       
+        protected LinkedList<keyValue<K, V>> GetLinkedList(int position)
+        {
+           
+            LinkedList<keyValue<K, V>> linkedList = items[position];
+           
+            if (linkedList == null)
             {
-
-                public k Key { get; set; }
-                public v Value { get; set; }
-
+                linkedList = new LinkedList<keyValue<K, V>>();
+                //adding a linkedlist in the given array position
+                items[position] = linkedList;
             }
-            private readonly int size;
-
-            private readonly LinkedList<keyValue<K, V>>[] items;
-
-
-            public MyMapNode(int size)
+            
+            return linkedList;
+        }
+        
+        public V Get(K key)
+        {
+            
+            int position = GetArrayPosition(key);
+            LinkedList<keyValue<K, V>> linkedList = GetLinkedList(position);
+            
+            foreach (keyValue<K, V> item in linkedList)
             {
-                this.size = size;
-
-                this.items = new LinkedList<keyValue<K, V>>[size];
-            }
-
-            protected int GetArrayPosition(K key)
-            {
-
-                int position = key.GetHashCode() % size;
-                return Math.Abs(position);
-            }
-
-            protected LinkedList<keyValue<K, V>> GetLinkedList(int position)
-            {
-
-                LinkedList<keyValue<K, V>> linkedList = items[position];
-
-                if (linkedList == null)
+               
+                if (item.Key.Equals(key))
                 {
-                    linkedList = new LinkedList<keyValue<K, V>>();
-
-                    items[position] = linkedList;
-                }
-
-                return linkedList;
-            }
-
-            public V Get(K key)
-            {
-
-                int position = GetArrayPosition(key);
-
-                LinkedList<keyValue<K, V>> linkedList = GetLinkedList(position);
-
-                foreach (keyValue<K, V> item in linkedList)
-                {
-
-                    if (item.Key.Equals(key))
-                    {
-                        return item.Value;
-                    }
-                }
-                return default(V);
-            }
-
-            public void Add(K key, V value)
-            {
-
-                int position = GetArrayPosition(key);
-
-                LinkedList<keyValue<K, V>> linkedList = GetLinkedList(position);
-
-                keyValue<K, V> item = new keyValue<K, V>() { Key = key, Value = value };
-
-                linkedList.AddLast(item);
-            }
-
-            public void Remove(K key)
-            {
-
-                int position = GetArrayPosition(key);
-
-                LinkedList<keyValue<K, V>> linkedList = GetLinkedList(position);
-                bool itemFound = false;
-
-                keyValue<K, V> foundItem = default(keyValue<K, V>);
-
-                foreach (keyValue<K, V> item in linkedList)
-                {
-                    if (item.Key.Equals(key))
-                    {
-                        itemFound = true;
-                        foundItem = item;
-                    }
-                }
-                if (itemFound)
-
-                {
-                    linkedList.Remove(foundItem);
+                    return item.Value;
                 }
             }
-
-            public void Display()
+            return default(V);
+        }
+        
+        public void Add(K key, V value)
+        {
+           
+            int position = GetArrayPosition(key);
+            LinkedList<keyValue<K, V>> linkedList = GetLinkedList(position);
+            
+            keyValue<K, V> item = new keyValue<K, V>() { Key = key, Value = value };
+           
+            linkedList.AddLast(item);
+        }
+        
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<keyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            keyValue<K, V> foundItem = default(keyValue<K, V>);
+            foreach (keyValue<K, V> item in linkedList)
             {
-
-                foreach (var linkedList in items)
+                if (item.Key.Equals(key))
                 {
-
-                    if (linkedList != null)
-
-                        foreach (keyValue<K, V> keyvalue in linkedList)
-                        {
-
-                            Console.WriteLine(keyvalue.Key + "\t" + keyvalue.Value);
-                        }
+                    itemFound = true;
+                    foundItem = item;
                 }
+            }
+            //if item is found, it is removed
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
             }
         }
+       
+        public void Display()
+        {
+            
+            foreach (var linkedList in items)
+            {
+               
+                if (linkedList != null)
+                    foreach (keyValue<K, V> keyvalue in linkedList)
+                    {
+                       
+                        Console.WriteLine(keyvalue.Key + "\t" + keyvalue.Value);
+                    }
+            }
+        }
+    }
+   
+    public struct keyValue<k, v>
+    {
+
+        public k Key { get; set; }
+        public v Value { get; set; }
 
     }
-
+}
 
--- Hashtable/Program.cs
+++ Hashtable/Program.cs
@@ -11,6 +11,18 @@ namespace Hashtable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hashtable");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            // Adding values in hashtable.
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "To");
+            hash.Add("5", "be");
+            // getting the specific value from hashtable.
+            //string hash5 = hash.Get("5");
+            hash.Remove("0");
+            hash.Display();
         }
     }
 }
