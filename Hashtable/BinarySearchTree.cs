8f8f65d0425a183bc7df02cb1c280440d1a8b1a6
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-08 19:53:07 +0530

    [Priyanshu] modified create binary search tree

---
+++ Hashtable/BinarySearchTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashtable
+{
+    class BinarySearchTree<T> where T:IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> Left { get; set; }
+        public BinarySearchTree<T> Right { get; set; }
+        public BinarySearchTree(T nodedata)
+        {
+            NodeData = nodedata;
+            Left = null;
+            Right = null;
+        }
+
+        public void Insert(T item)
+        {
+            T currentValue = NodeData;
+            if (currentValue.CompareTo(item) > 0)
+            {
+                if (Left == null)
+                {
+                    Left = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    Left.Insert(item);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    Right.Insert(item);
+                }
+            }
+        }
+
+        int leftCount = 0;
+        int rightCount = 0;
+        public void Display()
+        {
+            if (Left != null)
+            {
+                leftCount++;
+                Left.Display();
+            }
+            Console.WriteLine(NodeData);
+            if (Left != null)
+            {
+                rightCount++;
+                Right.Display();
+            }
+        }
+    }
+}
+
--- Hashtable/Program.cs
+++ Hashtable/Program.cs
@@ -6,67 +6,16 @@ using System.Threading.Tasks;
 
 namespace Hashtable
 {
-   public class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Hashtable");
-           // MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            // Adding values in hashtable.
-            //hash.Add("0", "To");
-            //hash.Add("1", "be");
-            //hash.Add("2", "or");
-            //hash.Add("3", "not");
-            //hash.Add("4", "To");
-            //hash.Add("5", "be");
-            // getting the specific value from hashtable.
-            //string hash5 = hash.Get("5");
-            //hash.Remove("0");
-            //hash.Display();
-
-
-           
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-
-            string input = "“paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            Console.WriteLine("Input Statement is: ");
-            Console.WriteLine(input);
-            string[] inputArray = input.Split(" ");
-            foreach (string word in inputArray)
-            {
-                if (hash.Get(word) == 0)
-                {
-                    hash.Add(word, 1);
-
-                }
-                else
-                {
-                    int value = hash.Get(word) + 1;
-                    hash.Set(word, value);
-                }
-            }
-
-            foreach (string word in inputArray)
-            {
-                if (hash.Get(word) == 0)
-                {
-                    hash.Remove(word, 1);
-
-                }
-                else
-                {
-                    int value = hash.Get(word) + 1;
-                    hash.Set(word, value);
-                }
-            }
-            string[] newInputArray = inputArray.Distinct().ToArray();
-            foreach (var word in newInputArray)
-            {
-                Console.WriteLine("The frequency of occurrence of \"" + word + "\" is: " + hash.Get(word));
-            }
+            Console.WriteLine("Binary Search Tree!");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
+            Console.ReadLine();
         }
     }
 }
-
-
-
