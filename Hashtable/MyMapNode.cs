a713091c268de2b618d349f2606c5ca195c907d3
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-08 19:23:16 +0530

    [Priyanshu] added frequency in paragraph

--- Hashtable/MyMapNode.cs
+++ Hashtable/MyMapNode.cs
@@ -7,51 +7,58 @@ using System.Threading.Tasks;
 namespace Hashtable
 {
 
-    public class MyMapNode<K, V>  //initializing the hashtable
+    public class MyMapNode<K, V>  //initializing the hashtable class
     {
+        public struct keyValue<k, v>
+        {
+
+            public k Key { get; set; }
+            public v Value { get; set; }
+
+        }
         private readonly int size;
-        
+
         private readonly LinkedList<keyValue<K, V>>[] items;
 
-        
+
         public MyMapNode(int size)
         {
             this.size = size;
             //array of linkedlist size is defined
             this.items = new LinkedList<keyValue<K, V>>[size];
         }
-       
+
         protected int GetArrayPosition(K key)
         {
-           
+
             int position = key.GetHashCode() % size;
             return Math.Abs(position);
         }
-       
+
         protected LinkedList<keyValue<K, V>> GetLinkedList(int position)
         {
-           
+
             LinkedList<keyValue<K, V>> linkedList = items[position];
-           
+
             if (linkedList == null)
             {
                 linkedList = new LinkedList<keyValue<K, V>>();
                 //adding a linkedlist in the given array position
                 items[position] = linkedList;
             }
-            
+
             return linkedList;
         }
-        
-        public V Get(K key)
+
+        protected V Get(K key)
         {
-            
+
             int position = GetArrayPosition(key);
             LinkedList<keyValue<K, V>> linkedList = GetLinkedList(position);
-            
+
             foreach (keyValue<K, V> item in linkedList)
             {
-               
+
                 if (item.Key.Equals(key))
                 {
                     return item.Value;
@@ -59,18 +66,18 @@ namespace Hashtable
             }
             return default(V);
         }
-        
+
         public void Add(K key, V value)
         {
-           
+
             int position = GetArrayPosition(key);
             LinkedList<keyValue<K, V>> linkedList = GetLinkedList(position);
-            
+
             keyValue<K, V> item = new keyValue<K, V>() { Key = key, Value = value };
-           
+
             linkedList.AddLast(item);
         }
-        
+
         public void Remove(K key)
         {
             int position = GetArrayPosition(key);
@@ -91,29 +98,70 @@ namespace Hashtable
                 linkedList.Remove(foundItem);
             }
         }
-       
+
         public void Display()
         {
-            
+
             foreach (var linkedList in items)
             {
-               
+
                 if (linkedList != null)
                     foreach (keyValue<K, V> keyvalue in linkedList)
                     {
-                       
+
                         Console.WriteLine(keyvalue.Key + "\t" + keyvalue.Value);
                     }
             }
         }
-    }
-   
-    public struct keyValue<k, v>
-    {
 
-        public k Key { get; set; }
-        public v Value { get; set; }
+        internal void Set(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            var linkedList = GetLinkedList(position);
+            KeyValue<K, V> temp = new KeyValue<K, V>();
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    temp = item;
+                }
+            }
+            temp.Value = value;
+        }
 
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
     }
+
+
+
 }
-
+}
--- Hashtable/Program.cs
+++ Hashtable/Program.cs
@@ -6,25 +6,54 @@ using System.Threading.Tasks;
 
 namespace Hashtable
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hashtable");
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+           // MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
             // Adding values in hashtable.
-            hash.Add("0", "To");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "To");
-            hash.Add("5", "be");
+            //hash.Add("0", "To");
+            //hash.Add("1", "be");
+            //hash.Add("2", "or");
+            //hash.Add("3", "not");
+            //hash.Add("4", "To");
+            //hash.Add("5", "be");
             // getting the specific value from hashtable.
             //string hash5 = hash.Get("5");
-            hash.Remove("0");
-            hash.Display();
+            //hash.Remove("0");
+            //hash.Display();
+
+
+           
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+
+            string input = "“paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            Console.WriteLine("Input Statement is: ");
+            Console.WriteLine(input);
+            string[] inputArray = input.Split(" ");
+            foreach (string word in inputArray)
+            {
+                if (hash.Get(word) == 0)
+                {
+                    hash.Add(word, 1);
+
+                }
+                else
+                {
+                    int value = hash.Get(word) + 1;
+                    hash.Set(word, value);
+                }
+            }
+
+            string[] newInputArray = inputArray.Distinct().ToArray();
+            foreach (var word in newInputArray)
+            {
+                Console.WriteLine("The frequency of occurrence of \"" + word + "\" is: " + hash.Get(word));
+            }
         }
     }
 }
+
 
 
